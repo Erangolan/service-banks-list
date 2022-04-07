00a7b811f4c951f7b81ccb3a5788bdb708b87546
Erangolan <37651948+Erangolan@users.noreply.github.com>
2022-04-07 10:27:17 +0300

    make it better

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'POST', 'GET')
     return res.status(200).json({})
